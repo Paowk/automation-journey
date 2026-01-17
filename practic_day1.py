8f2d6c30d444bcb426f3365e66c6e95165ed437a
Woranaat Kornpoch <woranaatk56@gmail.com>
2026-01-17 15:39:01 +0700

    Practic Day1: basic system runner

---
+++ practic_day1.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+today = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+system_name = "Automation Practice System"
+
+print(system_name)
+print("Date: ",today)
+print("STATUS: SYSTEM READY")
